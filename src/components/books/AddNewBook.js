08070b731eaee0a473db4302ae981f7838380535
alec-david <ard72@case.edu>
2018-06-07 13:23:49 -0400

    removed http links so connection secure

--- src/components/books/AddNewBook.js
+++ src/components/books/AddNewBook.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { Card, Image } from 'semantic-ui-react';
-import plusImg from '../../imgs/plus.svg';
+// import plusImg from '../../imgs/plus.svg';
+import plusImg from '../../imgs/plus6.png'
 
 const imageStyle = {
   cursor: 'pointer',
